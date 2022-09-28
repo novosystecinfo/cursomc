d1ef25d53922c17061ea8862eabc748fbde93846
Novosys <desenvolvimento@novosys.com.br>
2022-09-28 13:00:37 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/novosys/cursomc/domain/Pedido.java
+++ src/main/java/com/novosys/cursomc/domain/Pedido.java
@@ -17,7 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -28,13 +27,11 @@ public class Pedido implements Serializable {
 	private Integer id;
 	
 	@JsonFormat(pattern="dd/MM/yyyy hh:mm")
-	private Date instante;
+	private Date instante;	
 	
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
-	
-	@JsonManagedReference
+		
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
